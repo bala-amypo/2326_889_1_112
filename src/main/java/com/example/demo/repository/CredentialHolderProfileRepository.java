be623885528e9a36b7eb6785c9dd7a822e779865
bala-amypo <bala@amypo.in>
2025-12-18 08:31:08 +0000

    User commit

--- src/main/java/com/example/demo/repository/CredentialHolderProfileRepository.java
+++ src/main/java/com/example/demo/repository/CredentialHolderProfileRepository.java
@@ -3,7 +3,7 @@ package com.example.demo.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.example.demo.entity.CredentialHolderEntity;
+import com.example.demo.entity.CredentialHolderProfile;
 @Repository
-public interface StudentRepo extends JpaRepository<CredentialHolderProfile, Long>{
+public interface CredentialHolderProfileRepository extends JpaRepository<CredentialHolderProfile, Long>{
 }
