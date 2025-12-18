abbc18ea36501e8915ad4159e556ded96adce38b
bala-amypo <bala@amypo.in>
2025-12-18 13:58:51 +0000

    User commit

---
+++ src/main/java/com/example/demo/controller/CredentialRecordController.java
@@ -0,0 +1,43 @@
+package com.example.demo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import com.example.demo.entity.CredentialRecord;
+import com.example.demo.service.CredentialRecordService;
+
+@RestController
+@RequestMapping("/api/credential-records")
+public class CredentialRecordController {
+
+    @Autowired
+    private CredentialRecordService service;
+
+    @PostMapping
+    public CredentialRecord save(@RequestBody CredentialRecord credential) {
+        return service.save(credential);
+    }
+
+    @GetMapping
+    public List<CredentialRecord> getAll() {
+        return service.getAll();
+    }
+
+    @GetMapping("/{id}")
+    public CredentialRecord getById(@PathVariable Long id) {
+        return service.getById(id);
+    }
+
+    @PutMapping("/{id}")
+    public CredentialRecord update(@PathVariable Long id,
+                                   @RequestBody CredentialRecord credential) {
+        return service.update(id, credential);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+        service.delete(id);
+    }
+}
