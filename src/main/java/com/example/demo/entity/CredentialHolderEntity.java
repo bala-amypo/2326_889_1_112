35a1432d9206247a1dab8d097234a17d89a4cc84
bala-amypo <bala@amypo.in>
2025-12-18 06:31:51 +0000

    User commit

--- src/main/java/com/example/demo/entity/CredentialHolderEntity.java
+++ src/main/java/com/example/demo/entity/CredentialHolderEntity.java
@@ -1,13 +1,83 @@
-package com.example.demo.entity
-public class  CredentialHolderEntity{
-   @Id
-   private Long Id;
-   private String holderld;
-   private String fullName;
-   
-   private String email;
-   private String Organization;
-   private Boolean active;
-   private LocalDateTime createdAt;
+package com.example.demo.entity;
+import java.util.*
+import java.time.LocalDateTime;
 
+public class CredentialHolderProfile {
+
+    private Long id;
+    private String holderId;
+    private String fullName;
+    private String email;
+    private String organization;
+    private Boolean active;
+    private LocalDateTime createdAt;
+
+    public CredentialHolderProfile() {
+    }
+
+    public CredentialHolderProfile(Long id, String holderId, String fullName,String email, String organization,Boolean active, LocalDateTime createdAt) {
+        this.id = id;
+        this.holderId = holderId;
+        this.fullName = fullName;
+        this.email = email;
+        this.organization = organization;
+        this.active = active;
+        this.createdAt = createdAt;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getHolderId() {
+        return holderId;
+    }
+
+    public void setHolderId(String holderId) {
+        this.holderId = holderId;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getOrganization() {
+        return organization;
+    }
+
+    public void setOrganization(String organization) {
+        this.organization = organization;
+    }
+
+    public Boolean getActive() {
+        return active;
+    }
+
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
 }
