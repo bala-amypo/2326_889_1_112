5af1ef39d04a2385d4833ed9f93dd2545a97f3dc
bala-amypo <bala@amypo.in>
2025-12-18 14:01:52 +0000

    User commit

---
+++ src/main/java/com/example/demo/repository/CredentialRecordRepository.java
@@ -0,0 +1,8 @@
+package com.example.demo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.demo.entity.CredentialRecord;
+
+public interface CredentialRecordRepository
+        extends JpaRepository<CredentialRecord, Long> {
+}
