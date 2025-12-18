10670cc1b281d02e7b236a75aa8819826e3a4f12
bala-amypo <bala@amypo.in>
2025-12-18 06:24:03 +0000

    User commit

--- src/main/java/com/example/demo/entity/CredentialHolderEntity.java
+++ src/main/java/com/example/demo/entity/CredentialHolderEntity.java
@@ -1,4 +1,13 @@
 package com.example.demo.entity
 public class  CredentialHolderEntity{
+   @Id
+   private Long Id;
+   private String holderld;
+   private String fullName;
    
+   private String email;
+   private String Organization;
+   private Boolean active;
+   private LocalDateTime createdAt;
+
 }
