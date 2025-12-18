b84b33009c477e1df514b0b227cc99459425fe22
bala-amypo <bala@amypo.in>
2025-12-18 08:46:24 +0000

    User commit

--- src/main/java/com/example/demo/controller/CredentialHolderController.java
+++ src/main/java/com/example/demo/controller/CredentialHolderController.java
@@ -1,4 +1,4 @@
-package com.example.home.controller;
+package com.example.demo.controller
 import java.util.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
