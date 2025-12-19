f9b953ae10fddd985e65058d69f13e64173f8d25
bala-amypo <bala@amypo.in>
2025-12-19 05:26:14 +0000

    User commit

--- src/main/java/com/example/demo/service/serviceimpl/CredentialHolderProfileServiceImpl.java
+++ src/main/java/com/example/demo/service/serviceimpl/CredentialHolderProfileServiceImpl.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.example.demo.entity.CredentialHolderProfile;
 import com.example.demo.repository.CredentialHolderProfileRepository;
-import com.example.demo.service.CredentialHolderProfilerService;
+import com.example.demo.service.CredentialHolderProfileService;
 
 @Service
 public class CredentialHolderProfileServiceImpl implements CredentialHolderProfileService {
