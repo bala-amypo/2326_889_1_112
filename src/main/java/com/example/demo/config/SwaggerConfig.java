d6f78d06a63516f3f11d775a9a34cd20f81fbb67
bala-amypo <bala@amypo.in>
2025-12-26 12:18:24 +0000

    User commit

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -1,19 +1,21 @@
 package com.example.demo.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
-    
+
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
-            .info(new Info()
-                .title("Digital Credential Verification Engine API")
-                .version("1.0")
-                .description("API for managing digital credentials and verification"));
-    }
+                // You need to change the port as per your server
+                .servers(List.of(
+                        new Server().url("https://9295.pro604cr.amypo.ai/")
+                ));
+        }
 }
+
