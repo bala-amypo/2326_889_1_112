4ed33160ce99c3f4918fe8a9a85293940bc566b1
bala-amypo <bala@amypo.in>
2025-12-27 07:27:46 +0000

    User commit

--- src/main/java/com/example/demo/service/AuditTrailService.java
+++ src/main/java/com/example/demo/service/AuditTrailService.java
@@ -4,10 +4,7 @@ import com.example.demo.entity.AuditTrailRecord;
 import java.util.List;
 
 public interface AuditTrailService {
-
     AuditTrailRecord logEvent(AuditTrailRecord record);
-
     List<AuditTrailRecord> getLogsByCredential(Long credentialId);
-
     List<AuditTrailRecord> getAllLogs();
 }
